521a9706e1c2c4b9ee4d5c1746bda25c794b0d8f
André Kurfürst <andre@uxact.ly>
2022-06-08 15:25:42 +0200

    feat: fixed categories
    Add book shelf categories for using in different components

---
+++ starter/src/utils/categories.js
@@ -0,0 +1,7 @@
+const categories = [
+    { id: 'currentlyReading', title: 'Currently reading' },
+    { id: 'wantToRead', title: 'Want to read' },
+    { id: 'read', title: 'Read' }
+];
+
+export default categories;
