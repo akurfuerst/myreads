4e0b782c06e96c5b7b224c486ee44415b9433f69
André Kurfürst <andre@uxact.ly>
2022-06-08 15:31:22 +0200

    feat: book item
    Book item with book information and category switch

---
+++ starter/src/BookItem.js
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import categories from './utils/categories';
+
+const BookItem = ({ data, activeCategory, onUpdate }) => {
+    const currentCategory = activeCategory || 'none';
+
+    const onHandleUpdate = (event) => {
+        onUpdate(data, event.target.value);
+    };
+
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: data?.imageLinks?.smallThumbnail !== undefined ? `url(${data?.imageLinks?.smallThumbnail})` : 'unset',
+                        }}
+                    />
+                    <div className="book-shelf-changer">
+                        <select defaultValue={currentCategory} onChange={onHandleUpdate}>
+                            <option disabled>
+                                Move to...
+                            </option>
+                            {categories.map(category => {
+                                return (
+                                    <option key={category.id} value={category.id}>
+                                        {category.title}
+                                    </option>
+                                );
+                            })}
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{data.title}</div>
+                <div className="book-authors">{data.authors && data.authors.join(', ')}</div>
+                <Link className="book-button" to={`book/${data.id}`}>Details</Link>
+            </div>
+        </li>
+    );
+};
+
+BookItem.propTypes = {
+    data: PropTypes.object.isRequired,
+    activeCategory: PropTypes.string,
+    onUpdate: PropTypes.func.isRequired
+};
+
+export default BookItem;
